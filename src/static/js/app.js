536ad30530a5f4208d8e0c6ffae9a3b9eccd2e8d
Opod <48951218+DylanL0ng@users.noreply.github.com>
2023-10-25 16:57:02 +0100

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,15 +1,34 @@
 function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
-        <Container> 
-            <Row>
-                <Col md={{ offset: 3, span: 6 }}>
-                    <TodoListCard />
-                </Col>
-            </Row>
-        </Container>
+	    <Container>
+	       <Container> 
+	            <Row>
+	                <Col md={{ offset: 3, span: 6 }}>
+	                    <TodoListCard />
+	                </Col>
+	            </Row>
+	        </Container>
+		   <Container>
+			<YoutubeEmbed/>
+		    </Container>
+	    </Container>
     );
 }
+
+const YoutubeEmbed = ({ embedId }) => (
+    <div className="video-responsive">
+      <iframe
+        width="853"
+        height="480"
+        src={`https://www.youtube.com/embed/LxHiCZCKwa8`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
 
 function TodoListCard() {
     const [items, setItems] = React.useState(null);
